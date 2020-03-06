9ca4a055e74819b03ce06bcdf8c8b62d3d35ce44
hadeoh <usthmandanfodio@gmail.com>
2020-03-06 04:10:42 -0800

    First Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Github Finder</h1>
+      <p>App to search some Github folks</p>
+      <p>Version 1.0.0</p>
+    </div>
+  )
+}
+
+export default About;
